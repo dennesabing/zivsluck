b6d4d97671a11c54a3ba93f37c11e2c21af9ec15
dennesabing <dennes.b.abing@gmail.com>
2016-02-20 13:56:12 +0800

    Zivsluck Updates

--- config/fonts.php
+++ config/fonts.php
@@ -19,86 +19,146 @@ return [
 			'angel-tears' => [
 				'file' => 'angel-tears.ttf',
 				'name' => 'Angel Tears',
-				'enable' => true,
+				'enable' => false,
 				'fontsize' => 60,
-				'chaingroup' => ['pwc']
+			],
+			'anastasia' => [
+				'file' => 'anastasia.ttf',
+				'name' => 'Anastasia',
+				'enable' => true,
+				'fontsize' => 35,
+			],
+			'aroha' => [
+				'file' => 'aroha.ttf',
+				'name' => 'Aroha',
+				'enable' => true,
+				'fontsize' => 55,
+			],
+			'ave-utan' => [
+				'file' => 'ave-utan.ttf',
+				'name' => 'Ave Utan',
+				'enable' => true,
+				'fontsize' => 25,
 			],
 			'brush-script' => [
 				'file' => 'brush-script.ttf',
 				'name' => 'Soho Script',
 				'enable' => true,
 				'fontsize' => 35,
-				'chaingroup' => ['pwc']
+			],
+			'blackfat' => [
+				'file' => 'Blackfat.ttf',
+				'name' => 'Blackfat',
+				'enable' => true,
+				'fontsize' => 25,
+			],
+			'BukhariScript' => [
+				'file' => 'BukhariScript.ttf',
+				'name' => 'Bukhari Script',
+				'enable' => true,
+				'fontsize' => 25,
 			],
 			'classic-cursive' => [
 				'file' => 'classic-cursive.ttf',
 				'name' => 'Classic Cursive',
 				'enable' => true,
 				'fontsize' => 40,
-				'chaingroup' => ['pwc']
 			],
 			'commercial-script' => [
 				'file' => 'commercial-script.ttf',
 				'name' => 'Commercial Script',
 				'enable' => true,
 				'fontsize' => 35,
-				'chaingroup' => ['pwc']
 			],
 			'deftone' => [
 				'file' => 'deftone-stylus.ttf',
 				'name' => 'Deftone Stylus',
 				'enable' => true,
 				'fontsize' => 30,
-				'chaingroup' => ['pwc']
+			],
+			'guldscript' => [
+				'file' => 'guld-script.ttf',
+				'name' => 'Guld Script',
+				'enable' => true,
+				'fontsize' => 40,
+			],
+			'Khadija' => [
+				'file' => 'Khadija.ttf',
+				'name' => 'Khadija',
+				'enable' => true,
+				'fontsize' => 35,
 			],
 			'lauren-script' => [
 				'file' => 'lauren-script.ttf',
 				'name' => 'Lauren Script',
 				'enable' => true,
 				'fontsize' => 25,
-				'chaingroup' => ['pwc']
 			],
 			'canterbury' => [
 				'file' => 'Canterbury.ttf',
 				'name' => 'Canterbury',
 				'enable' => true,
 				'fontsize' => 35,
-				'chaingroup' => ['pwc']
+			],
+			'LoveRock' => [
+				'file' => 'LoveRock.ttf',
+				'name' => 'Love Rock',
+				'enable' => true,
+				'fontsize' => 35,
+			],
+			'masterofbreak' => [
+				'file' => 'master-of-break.ttf',
+				'name' => 'Master of Break',
+				'enable' => true,
+				'fontsize' => 35,
 			],
 			'perfect-penmanship' => [
 				'file' => 'perfect-penmanship.ttf',
 				'name' => 'Perfect Penmanship',
 				'enable' => true,
 				'fontsize' => 30,
-				'chaingroup' => ['pwc']
+			],
+			'raisinsdessables' => [
+				'file' => 'raisin-des-sables.ttf',
+				'name' => 'Raisins Des Sables',
+				'enable' => true,
+				'fontsize' => 50,
 			],
 			'scriptina' => [
 				'file' => 'scriptina.ttf',
 				'name' => 'Scriptina',
 				'enable' => true,
 				'fontsize' => 35,
-				'chaingroup' => ['victoria'],
+			],
+			'Scriptonite' => [
+				'file' => 'Scriptonite.ttf',
+				'name' => 'Scriptonite',
+				'enable' => true,
+				'fontsize' => 35,
 			],
 			'swirl-print' => [
 				'file' => 'swirl-print.ttf',
 				'name' => 'Swirl Print',
 				'enable' => true,
 				'fontsize' => 30,
-				'chaingroup' => ['pwc']
 			],
 			'valentines' => [
 				'file' => 'valentines.ttf',
 				'name' => 'Valentine\'s',
 				'enable' => true,
 				'fontsize' => 35,
-				'chaingroup' => ['pwc']
 			],
 			'verdana' => [
 				'file' => 'verdana.ttf',
 				'name' => 'Verdana',
 				'enable' => true,
 				'fontsize' => 23,
-				'chaingroup' => ['victoria']
+			],
+			'wasted' => [
+				'file' => 'wasted.ttf',
+				'name' => 'Wasted',
+				'enable' => true,
+				'fontsize' => 30,
 			],
 		],
 	],
